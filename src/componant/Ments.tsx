13e5657982ad0a1b3ab262122cae56d731047673
earth <konthakarn_f@cmu.ac.th>
2021-09-09 14:53:18 +0700

    like

--- src/componant/Ments.tsx
+++ src/componant/Ments.tsx
@@ -2,25 +2,44 @@ import { CommentType } from "../data/comments"
 import Replies from "./Replies"
 
 const Ments = ({ username, userImagePath, commentText, likeNum, replies }: CommentType) => {
-    return (
-        <div>
-            <div className="flex p-2 items-start space-x-2">
-                <img className="w-10 w-10 rounded-full" src={userImagePath}></img>
-                <div className="bg-gray-500 rounded-lg p-2">
-                    <p className="font-semibold text-white">{username}</p>
-                    <p className='text-white'>{commentText}</p>
-                    {/* like section (จะไม่แสดงถ้าไม่มีใครไลค์เลย) */}
-                    <div className='flex items-center'>
-                        <img className='w-4 h-4 mr-1' src='/like.svg'></img>
-                        <p className='text-gray-300'>{likeNum}</p>
+    if(likeNum>0){
+        return (
+            <div>
+                <div className="flex p-2 items-start space-x-2">
+                    <img className="w-10 w-10 rounded-full" src={userImagePath}></img>
+                    <div className="bg-gray-500 rounded-lg p-2">
+                        <p className="font-semibold text-white">{username}</p>
+                        <p className='text-white'>{commentText}</p>
+                        {/* like section (จะไม่แสดงถ้าไม่มีใครไลค์เลย) */}
+                        <div className='flex items-center'>
+                            <img className='w-4 h-4 mr-1' src='/like.svg'></img>
+                            <p className='text-gray-300'>{likeNum}</p>
+                        </div>
                     </div>
                 </div>
+                {replies.map(x => <Replies username={x.username} userImagePath={x.userImagePath} commentText={x.commentText} likeNum={x.likeNum} replies={x.replies}></Replies>)}
             </div>
-            {replies.map(x => <Replies username={x.username} userImagePath={x.userImagePath} commentText={x.commentText} likeNum={x.likeNum} replies={x.replies}></Replies>)}
-        </div>
-
-
-    )
+    
+    
+        )
+    }else{
+        return (
+            <div>
+                <div className="flex p-2 items-start space-x-2">
+                    <img className="w-10 w-10 rounded-full" src={userImagePath}></img>
+                    <div className="bg-gray-500 rounded-lg p-2">
+                        <p className="font-semibold text-white">{username}</p>
+                        <p className='text-white'>{commentText}</p>
+                        {/* like section (จะไม่แสดงถ้าไม่มีใครไลค์เลย) */}
+                    </div>
+                </div>
+                {replies.map(x => <Replies username={x.username} userImagePath={x.userImagePath} commentText={x.commentText} likeNum={x.likeNum} replies={x.replies}></Replies>)}
+            </div>
+    
+    
+        )
+    }
+    
 
 }
 
